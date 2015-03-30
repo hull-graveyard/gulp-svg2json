f8314b428a9ac364424516e2e7740231a30fd1c3
Romain Dardour <romain@hull.io>
2015-03-31 00:22:49 +0200

    Generate individual icons and index.js recap file

--- index.js
+++ index.js
@@ -1,83 +1,91 @@
 "use strict";
+
 /*global module, require*/
 var _ = require("underscore"),
+    through = require("through2"),
+    htmlclean = require("htmlclean"),
+    parse = require("jsonml-parse"),
     camelize = require("camelize"),
     gutil = require("gulp-util"),
-    Stream = require("readable-stream"),
-    cheerio = require("cheerio"),
     path = require("path");
 
-function svg2object(data){
-  var $ = cheerio.load(data);
-  var svg = _.pick($("svg").attribs, "viewbox");
-  var paths = _.map($("path"), function(p){
-    return _.pick(p.attribs, "d", "stroke-linecap");
-  });
-  return camelize({svg: svg, path: paths});
-}
 
+var VALID_ATTRIBUTES=["viewBox", "id", "stroke", "strokewidth", "fill", "fillrule", "transform", "d", "strokelinecap"];
+// var VALID_ATTRIBUTES=["d"];
 module.exports = function(options) {
+
   var files = [];
-  var stream = new Stream.Transform({objectMode: true});
-
+  var stream = through.obj(processSvg, flush);
   options = options || {};
-  options.fileName = options.fileName || "out.json";
 
-  options.log   = options.log   || function(){
-    gutil.log.apply(gutil, ["gulp-svg2json:"].concat([].slice.call(arguments, 0).concat()));
-  };
-  options.error = options.error || function(){
-    stream.emit("error", new gutil.PluginError("svgicons2svgfont", [].slice.call(arguments, 0).concat()));
-  };
+  if(!options.fileName){
+    console.log("Error");
+  }
+  function parseJsonml(data){
+    if(_.isArray(data)) {
+      return _.map(data, function(datum){
+        return parseJsonml(datum);
+      });
+    }
 
+    if ( _.isObject(data) ){
+      // Quick n dirty hack to fix htmlparser2's lowercasing of attributes;
+      if(data.viewbox){
+        data.viewBox = data.viewbox;
+        delete data.viewbox;
+      }
+      var boundPick = _.pick.bind(undefined, data);
+      var d = camelize(boundPick.apply(this, VALID_ATTRIBUTES));
+      return d;
+    }
 
-  // Collecting icons
-  stream._transform = function bufferContents(file, unused, done) {
-    // When null just pass through
-    // If the ext doesn't match, pass it through
-    if(file.isNull() || (!options.ignoreExt) && ".svg" !== path.extname(file.path)) { stream.push(file); done(); return;}
-    files.push(file);
-    done();
-  };
+    return data;
+  }
 
-  // Generating the font
-  stream._flush = function endStream(done){
-    // No icons, exit
+  function processSvg(file, encoding, done){
+    // Pass file through if:
+    // - file has no contents
+    // - file is a directory
+    if (file.isNull() || file.isDirectory()) {
+      this.push(file);
+      return done();
+    }
+    if(file.isBuffer()){
+      var parseStream = parse();
+      var content = new Buffer(htmlclean(file.contents.toString()));
+      parseStream.write(content);
+      parseStream.on("data", function(data){
+        var json = _.map(data, function(datum){ return parseJsonml(datum); });
+        file.contents = new Buffer(JSON.stringify(json, null, 2));
+        var output = new gutil.File({
+          cwd: file.cwd,
+          base: file.base,
+          path: path.join(file.base, path.basename(file.path, ".svg")+".json"),
+          contents: file.contents
+        });
+        files.push(output);
+        stream.push(output);
+      });
+    }
+    return done();
+  }
 
-    if (files.length === 0) { return done(); }
-    // Map each icons to their corresponding icons
-
-    var icons = files.map(function(file) {
-      // Creating an object for each icon
-      var matches = path.basename(file.path).match(/^(.*).svg$/i);
-
-      var glyph = {
-        name: matches[1],
-        file: file.path,
-        stream: file.pipe(new Stream.PassThrough()),
-        json: svg2object(String(file.contents))
-      };
-      console.log("Adding "+file.path);
-      return glyph;
+  function flush(done){
+      // var out = "module.exports={\n";
+    var exports =files.map(function(f){
+      var basename = path.basename(f.path, ".json");
+      return "  \""+basename+"\": require(\"./"+basename+".json\")";
     });
-
-    icons.forEach(function(glyph) { glyph.stream.on("end", function() {}); });
-
-    var joinedJson = _.reduce(icons, function(memo, f){
-      memo[f.name]=f.json;
-      return memo;
-    }, {});
-
-    var joinedFile = new gutil.File({
+    var out = "module.exports = {\n"+exports.join(",\n")+"\n};";
+    var output = new gutil.File({
       cwd: files[0].cwd,
       base: files[0].base,
       path: path.join(files[0].base, options.fileName),
-      contents: new Buffer(JSON.stringify(joinedJson, null, 2))
+      contents: new Buffer(out)
     });
-    stream.push(joinedFile);
-    done();
-  };
-
+    stream.push(output);
+    done()
+  }
 
   return stream;
 };
